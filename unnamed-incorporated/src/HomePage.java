82e3ecb35b7eaeb0971de24104035dcbcc5d4448
Brendan Buglewicz <bcbuglewicz@unomaha.edu>
2020-11-08 21:47:31 -0600

    Added the home/search page with a nav footer on the bottom. Can be improved with the way it is set up and will have to change the hyperlinks when we add the other HTMLs.

--- unnamed-incorporated/src/HomePage.java
+++ unnamed-incorporated/src/HomePage.java
@@ -1,5 +1,3 @@
-
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,19 +6,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/TestServlet")
-public class TestServlet extends HttpServlet {
+public class HomePage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public TestServlet() {
+    public HomePage() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 }
