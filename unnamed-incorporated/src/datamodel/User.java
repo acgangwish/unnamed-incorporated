1b594cec6a9388c235d416a2fe1a93185fb9f22f
A Gangwish <acgangwish@unomaha.edu>
2020-11-21 21:11:27 -0600

    Added User datamodel.

---
+++ unnamed-incorporated/src/datamodel/User.java
@@ -0,0 +1,68 @@
+package datamodel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "Users")
+public class User {
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column (name = "Id")
+	private int id;
+	
+	@Column (name = "Username")
+	private String username;
+	
+	@Column (name = "Password")
+	private String password;
+	
+	@Column (name = "Restaurant")
+	private boolean restaurant; // false for user, true for restaurant
+	
+	public User (int id, String username, String password, boolean restaurant) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.restaurant = restaurant;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(boolean restaurant) {
+		this.restaurant = restaurant;
+	}
+	
+	
+}
