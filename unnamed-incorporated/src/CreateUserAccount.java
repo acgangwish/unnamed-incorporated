e52f194b41e23d48277f597aee5bc0f8d32fa77a
michaelmenard <michaelmenard@localhost>
2020-12-14 20:30:52 -0600

    User Account Creation Working

--- unnamed-incorporated/src/CreateUserAccount.java
+++ unnamed-incorporated/src/CreateUserAccount.java
@@ -1,11 +1,14 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import util.utilDB;
 
 /**
  * Servlet implementation class CreateUserAccount
@@ -26,8 +29,38 @@ public class CreateUserAccount extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+	    //This is for the registering a new customer/restaurant
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+	    String usernameReg = request.getParameter("usernameReg");
+	    String passwordReg = request.getParameter("passwordReg");
+	    String confirmPass = request.getParameter("passwordRegConfirm");
+	    String outputReg = request.getParameter("registerType");
+	    
+	    boolean passCorrect = false;
+	    
+	    if (passwordReg.equals(confirmPass)) {
+	    	passCorrect = true;
+	    } else {
+	    	out.print("Error: Passwords do not match.");
+	    	request.getRequestDispatcher("SignIn.html").include(request, response);
+	    }
+	    
+	    if (passCorrect && outputReg.equals("cust")) {
+	    	utilDB.createUser(usernameReg, passwordReg, false);
+	    	request.getRequestDispatcher("SignIn.html").include(request, response);
+	    }
+	    
+	    if (passCorrect && outputReg.equals("rest")) {
+	    	utilDB.createUser(usernameReg, passwordReg, true);
+	    	request.getRequestDispatcher("CreateRestaurantInformation.html").include(request, response);
+	    }
+	    
+	    else {
+	    	out.print("Unknown Error.");
+	    	request.getRequestDispatcher("SignIn.html").include(request, response);
+	    }
 	}
 
 	/**
