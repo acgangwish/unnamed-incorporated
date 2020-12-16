d7fa9db21356e398b1ebec878bb4a19aa6a4fbd3
bbuglewicz <60749429+bbuglewicz@users.noreply.github.com>
2020-12-16 11:50:37 -0600

    Creating restuarant account

--- unnamed-incorporated/src/CreateUserAccount.java
+++ unnamed-incorporated/src/CreateUserAccount.java
@@ -2,12 +2,16 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import datamodel.User;
 import util.utilDB;
 
 /**
@@ -16,7 +20,7 @@ import util.utilDB;
 @WebServlet("/CreateUserAccount")
 public class CreateUserAccount extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    public int id = 0;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -29,7 +33,6 @@ public class CreateUserAccount extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 	    //This is for the registering a new customer/restaurant
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
@@ -37,6 +40,7 @@ public class CreateUserAccount extends HttpServlet {
 	    String passwordReg = request.getParameter("passwordReg");
 	    String confirmPass = request.getParameter("passwordRegConfirm");
 	    String outputReg = request.getParameter("registerType");
+	    List<User> users = utilDB.getRestaurantUserList();
 	    
 	    boolean passCorrect = false;
 	    
@@ -54,15 +58,25 @@ public class CreateUserAccount extends HttpServlet {
 	    
 	    if (passCorrect && outputReg.equals("rest")) {
 	    	utilDB.createUser(usernameReg, passwordReg, true);
+	    	
+	    	for (Iterator<?> iterator = users.iterator(); iterator.hasNext();) {
+		    	User user = (User) iterator.next();
+		    	if (user.getUsername().equals(usernameReg) && user.getPassword().equals(passwordReg)) {
+		    		id = user.getId();
+		    		break;
+		    	}
+		    }
+	    	
 	    	request.getRequestDispatcher("CreateRestaurantInformation.html").include(request, response);
-	    }
-	    
-	    else {
+	    } else {
 	    	out.print("Unknown Error.");
 	    	request.getRequestDispatcher("SignIn.html").include(request, response);
 	    }
 	}
 
+	public int getID() {
+		return id;
+	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
